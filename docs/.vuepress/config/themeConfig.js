0f4b88d4cebf890e8c2d5c46f67e24d68f335ff7
litongjava <litongjava@qq.com>
2024-01-28 17:52:18 -1000

    add right-slider

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -1,6 +1,13 @@
+const sidebarZh = require("./sidebar-zh.json");
+const sidebarEn = require("./sidebar-en.json");
 const navEn = require("./nav-en.json");
 
 module.exports = {
+  logo: "./logo.png",
   lastUpdated: "Last Updated", // string | boolean,K
   nav: navEn,
+  sidebar: {
+    "/zh/": sidebarZh,
+    "/en/": sidebarEn,
+  },
 };
