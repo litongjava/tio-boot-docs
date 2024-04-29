d56fd57ab247192ca6b30e272d79db61588949e0
litongjava <litongjava@qq.com>
2024-04-29 02:45:55 -1000

    add source

--- docs/.vuepress/config/nav-en.js
+++ docs/.vuepress/config/nav-en.js
@@ -1,1 +1,12 @@
-export default ["/", "/about"];
+export default [
+  "/",
+  "/about",
+  {
+    text: "Source",
+    icon: "info",
+    children: [
+      { text: "Github", link: "https://github.com/litongjava/tio-boot", icon: "githb" },
+      { text: "Gitee", link: "https://gitee.com/ppnt/tio-boot", icon: "gitee" },
+    ],
+  },
+];
