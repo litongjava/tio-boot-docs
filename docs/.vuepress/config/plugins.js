2f3cad6d946af06e0d94725a94703a6086da1f56
litongjava <litongjava@qq.com>
2024-04-29 02:16:39 -1000

    fix error for build

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,10 +1,11 @@
-import { searchPlugin } from "@vuepress/plugin-search";
-import { pwaPlugin } from "@vuepress/plugin-pwa";
-import { seoPlugin } from "@vuepress/plugin-seo";
+// import { searchPlugin } from "@vuepress/plugin-search";
+// import { pwaPlugin } from "@vuepress/plugin-pwa";
+// import { seoPlugin } from "@vuepress/plugin-seo";
 export default [
-  searchPlugin({}),
-  pwaPlugin({}),
-  seoPlugin({
-    // 选项
-  }),
+  // searchPlugin({}),
+  // pwaPlugin({}),
+  // seoPlugin({
+  //   hostname: "tio-boot-docs.vercel.app",
+  //   // 选项
+  // }),
 ];
