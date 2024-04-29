6d8900ac5dae235a8f685c63ac89b4aa4bd65ffb
litongjava <litongjava@qq.com>
2024-04-29 02:25:28 -1000

    add search plugin and seo plugin

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,11 +1,15 @@
 // import { searchPlugin } from "@vuepress/plugin-search";
 // import { pwaPlugin } from "@vuepress/plugin-pwa";
-// import { seoPlugin } from "@vuepress/plugin-seo";
+import { seoPlugin } from "@vuepress/plugin-seo";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 export default [
   // searchPlugin({}),
   // pwaPlugin({}),
-  // seoPlugin({
-  //   hostname: "tio-boot-docs.vercel.app",
-  //   // 选项
-  // }),
+  seoPlugin({
+    hostname: "tio-boot-docs.vercel.app",
+    // 选项
+  }),
+  searchProPlugin({
+    // your options
+  }),
 ];
