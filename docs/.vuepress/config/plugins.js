fbd3f0ff530415b7827b35a78d0278aa03bff2d1
litongjava <litongjava@qq.com>
2024-10-24 14:48:34 -1000

    update updatePopup

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -13,30 +13,15 @@ export default [
   }),
   pwaPlugin({
     serviceWorker: true,
-    manifest: {
-      name: "Tio Boot Docs",
-      short_name: "tio-boot",
-      lang: "zh-CN",
-      start_url: "/",
-      display: "standalone",
-      background_color: "#ffffff",
-      theme_color: "#3eaf7c",
-      icons: [
-        {
-          src: "/images/logo-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "/images/logo-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
-    },
     updatePopup: {
-      message: "发现新内容可用",
-      buttonText: "刷新",
+      "/": {
+        message: "New content is available.",
+        buttonText: "Refresh",
+      },
+      "/zh/": {
+        message: "发现新内容可用",
+        buttonText: "刷新",
+      },
     },
   }),
   // autoCatalogPlugin({
