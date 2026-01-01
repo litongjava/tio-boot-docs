87684fab20921720549ce8ebc87c83c9fba5f18a
litongjava <litongjava@qq.com>
2026-01-01 17:55:30 +0800

    update nav-en

--- docs/.vuepress/config/nav-en.js
+++ docs/.vuepress/config/nav-en.js
@@ -5,6 +5,9 @@ export default [
     children: [
       { text: "java-db", link: "/zh/09_java-db/01" },
       { text: "api-table", link: "/zh/10_api-table/01" },
+      { text: "mysql", link: "/zh/40_mysql/01" },
+      { text: "postgresql", link: "/zh/41_postgresql/01" },
+      { text: "oceanbase", link: "/zh/43_oceanbase/01" }
     ],
   },
   {
